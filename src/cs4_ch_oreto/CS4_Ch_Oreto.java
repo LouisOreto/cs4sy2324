4842cdfa5109cd5f514e0cbe1b028f79a4215487
CHARM <CHARM@172.22.16.114>
2023-08-29 16:13:54 +0800

    first java project

---
+++ src/cs4_ch_oreto/CS4_Ch_Oreto.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4_ch_oreto;
+
+/**
+ *
+ * @author CHARM
+ */
+public class CS4_Ch_Oreto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name = "Louis Oreto";
+        byte age = 16;
+        char section = 'c';
+        System.out.println("Hi, my name is "+name+"! And I'm "+age+" years old. ");
+        
+    }
+    
+}
