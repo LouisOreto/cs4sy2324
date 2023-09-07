cbc5e2176163c8d947c6790b20df501223b75ffe
LouisOreto <112600647+LouisOreto@users.noreply.github.com>
2023-09-07 15:42:04 +0800

    Update CS4_Ch_Oreto.java

--- src/cs4_ch_oreto/CS4_Ch_Oreto.java
+++ src/cs4_ch_oreto/CS4_Ch_Oreto.java
@@ -15,12 +15,44 @@ public class CS4_Ch_Oreto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        String name = "Louis Oreto";
-        byte age = 16;
-        char section = 'c';
-        System.out.println("Hi, my name is "+name+"! And I'm "+age+" years old. ");
+        String kname = "Kafka";
+        byte klvl = 70;
+        short katk = 3087;
+                
+        String gname = "Gepard";
+        byte glvl = 70;
+        short gatk = 1249;
         
+        String iname = "Imbibitor Lunae";
+        byte ilvl = 50;
+        short iatk = 1614;
+        
+        boolean lvlcomp = klvl > ilvl;
+        boolean atkcomp = gatk > iatk;
+        boolean isKafkaLvl70 = true;
+        boolean isGepardLvl70 = true;
+        boolean isImbibitorLvl70 = false;
+        
+        boolean areAllCharsLvl70 = isKafkaLvl70 && isGepardLvl70 && isImbibitorLvl70;
+        
+        System.out.println("Stats for my team's main characters in the game Honkai Star Rail");
+        System.out.println("");
+        System.out.println("Character name: " + kname);
+        System.out.println("level: " + klvl);
+        System.out.println("attack: " + katk);
+        System.out.println("");
+        System.out.println("Character name: " + gname);
+        System.out.println("level: " + glvl);
+        System.out.println("attack: " + gatk);
+        System.out.println("");
+        System.out.println("Character name: " + iname);
+        System.out.println("level: " + ilvl);
+        System.out.println("attack: " + iatk);
+        System.out.println("");
+        System.out.println("Total levels: "+ (klvl+glvl+ilvl)); /* operator: sum */
+        System.out.println("Kafka's lvl is higher than Imbibitor Lunae: "+ lvlcomp);/* operator: comparison */
+        System.out.println("Gepard's atk is higher than Imbibitor Lunae: "+ atkcomp); /* operator: comparison */
+        System.out.println("Are all characters at level 70: "+ areAllCharsLvl70); /* operator: logical */
     }
     
 }
