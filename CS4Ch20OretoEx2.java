efa4e75821d338217027ca9fac4abc2ba584b684
LouisOreto <112600647+LouisOreto@users.noreply.github.com>
2023-09-17 20:35:27 +0800

    Create CS4Ch20OretoEx2.java

---
+++ CS4Ch20OretoEx2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cs4ch20oretoex2;
+
+/**
+ *
+ * @author Giselle
+ */
+public class CS4Ch20OretoEx2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JavaClass Kafka = new JavaClass("Kafka", 70, 3087);
+       JavaClass Gepard = new JavaClass("Gepard", 70, 1249);
+       JavaClass IL = new JavaClass("Imbibitor Lunae", 50, 1614);
+       Kafka.displayInfo();
+       Gepard.displayInfo();
+       IL.displayInfo();
+       
+    }
+    
+}
